a546929fe15d8d469d839307e22665f6e36ce227
Xiaorong Ma <mxiaoai@gmail.com>
2019-04-19 19:29:29 -0400

    Update Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-class App extends Component {
-  render() {
+
+const SubMenu = (props) => {
+  let tbody = props.items.map(item => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <tr key={item.id}>
+        <td>{item.name}</td>
+        <td>{item.description}</td>
+      </tr>
+    )
+  })
+
+  if (props.shortName === "") {
+    return null;
+  } else {
+    return (
+      <div>
+        <h1>Items in Categories:({props.shortName})</h1>
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+            </tr>
+            {tbody}
+          </tbody>
+        </table>
+      </div>
+    )
+
+  }
+}
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuList: [],
+      items: [],
+      shortName: ""
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://stream-restaurant-menu-svc.herokuapp.com/category")
+      .then(res => {
+        console.log(res.data);
+        this.setState({menuList: res.data});
+      })
+  }
+
+  downloadItems(shortName) {
+    console.log(shortName);
+    axios
+      .get(`https://stream-restaurant-menu-svc.herokuapp.com/item?category=${shortName}`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ items: res.data, shortName: shortName})
+      })
+  }
+
+  render() {
+    let menuList = this.state.menuList.map(menu => {
+      return (
+        <li 
+          key={menu.id}
+          onClick={() => this.downloadItems(menu.short_name)}
+        >
+        <div className="box">{menu.name} - {menu.short_name}</div>
+        </li>
+      )
+    });
+
+    return (
+      <div className="container">
+        <div className="left">
+          <h1>Menu Categories</h1>
+          <ul>{menuList}</ul>
+        </div>
+        <div className="right">
+          <SubMenu items={this.state.items} shortName={this.state.shortName}></SubMenu>
+        </div>
       </div>
     );
   }
